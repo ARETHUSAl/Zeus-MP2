cabd007335ee830c5e84abee98b3d2fbf7b15fdd
Johannes Reppin <johannesreppin@gmail.com>
2015-04-28 11:56:20 +0200

    deleted .h files from repo

--- src90/impsoln.h
+++
@@ -1,6 +0,0 @@
-      REAL totlsit, totnrit, nrpert, lspert, tot_trout, tot_tsr,
-     .     tot_tsyn, tot_tcom, lstime
-      common /impacct/ totlsit, totnrit, tot_trout, tot_tsr, 
-     .                 tot_tsyn, tot_tcom, lstime
-      integer cgerrcrit
-      common /cgerr/ cgerrcrit
